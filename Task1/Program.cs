6ea345811866e19c15021df743767cbc10bfa015
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-05 23:37:25 +0200

    Add Task 11

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,15 +3,15 @@
 while (isWork)
 {
     System.Console.WriteLine();
-    System.Console.WriteLine("1 - Task 1: Max digit of a random number between 10 and 99");
-    System.Console.WriteLine("2 - Task : ");
+    System.Console.WriteLine("1 - Task 08: Max digit of a random number between 10 and 99");
+    System.Console.WriteLine("2 - Task 11: First and last digits of a random 3-digit number");
     System.Console.WriteLine("0 - Exit");
     System.Console.Write("Enter a number of task: ");
 
     if (int.TryParse(Console.ReadLine(), out int taskNo))
     {
         System.Console.WriteLine();
-        
+
         switch (taskNo)
             {
                 case 1: // Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
@@ -32,6 +32,20 @@ while (isWork)
 
                     break;
                 }
+ 
+                case 2: // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+                {
+                    int num = new Random().Next(100, 1000);
+
+                    System.Console.WriteLine($"Random number: {num}");
+
+                    int res = num / 100 * 10 + num % 10;
+
+                    System.Console.WriteLine($"Number without second digit: {res}");
+
+                    break;
+                }
+ 
                 case 0:
                 {
                     isWork = false;
