01bba84de781ad55d6e200c1e009139fd64b634a
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-05 23:20:37 +0200

    Add swith task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,15 +1,43 @@
-﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+﻿bool isWork = true;
 
-int num = new Random().Next(10, 100);
-
-System.Console.WriteLine(num);
-
-int maxDigit = num % 10;
-
-while (num > 0)
+while (isWork)
 {
-    if (num % 10 > maxDigit) maxDigit = num % 10;
-    num /= 10;     
-}
+    System.Console.WriteLine();
+    System.Console.WriteLine("1 - Task 1: Max digit of a random number between 10 and 99");
+    System.Console.WriteLine("2 - Task : ");
+    System.Console.WriteLine("0 - Exit");
+    System.Console.Write("Enter a number of task: ");
 
-System.Console.WriteLine(maxDigit);
+    if (int.TryParse(Console.ReadLine(), out int taskNo))
+    {
+        System.Console.WriteLine();
+        
+        switch (taskNo)
+            {
+                case 1: // Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+                {
+                    int num = new Random().Next(10, 100);
+
+                    System.Console.WriteLine($"Random number: {num}");
+
+                    int maxDigit = num % 10;
+
+                    while (num > 0)
+                    {
+                        if (num % 10 > maxDigit) maxDigit = num % 10;
+                        num /= 10;     
+                    }
+
+                    System.Console.WriteLine($"Max digit: {maxDigit}");
+
+                    break;
+                }
+                case 0:
+                {
+                    isWork = false;
+                    break;
+                }
+            }
+    }
+
+}
