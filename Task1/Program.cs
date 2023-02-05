1990afbd0c5a17097f84f0f06e90320ffc16a4df
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-05 23:01:12 +0200

    Init commit

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10, 100);
+
+System.Console.WriteLine(num);
+
+int maxDigit = num % 10;
+
+while (num > 0)
+{
+    if (num % 10 > maxDigit) maxDigit = num % 10;
+    num /= 10;     
+}
+
+System.Console.WriteLine(maxDigit);
